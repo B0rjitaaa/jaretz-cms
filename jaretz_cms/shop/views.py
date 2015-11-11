d9196c7ee8967d33fb598f2a00aec5cd925ec278
Jaretz <bmcx12@gmail.com>
2015-11-12 00:12:01 +0100

    Performed basic structure of views.

--- jaretz_cms/shop/views.py
+++ jaretz_cms/shop/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+	pass
+
+
+def item(request, id_item):
+	pass
+
+
+def items(request):
+	pass
+
+
+def category(request, id_category):
+	pass
+
+
+def categories(request):
+	pass
