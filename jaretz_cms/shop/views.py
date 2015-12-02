a7d481a2d06ee081c0bd7586682275d0cb069ad5
Jaretz <bmcx12@gmail.com>
2015-12-02 16:40:18 +0100

    Fixed footer information.

--- jaretz_cms/shop/views.py
+++ jaretz_cms/shop/views.py
@@ -33,7 +33,8 @@ def item(request, item_slug, item_id):
 	response = {
 		'shop_info': Shop.objects.all()[0],
 		'item': item,
-		'items': item.category.items.all().exclude(reference_code=item.reference_code)
+		'items': item.category.items.all().exclude(reference_code=item.reference_code),
+		'categories': Category.objects.all().filter(parent_category=None),
 	}
 	return render (request, 'item.html', response)
 
@@ -49,7 +50,8 @@ def items(request):
 def category(request, category_slug, category_id):
 	response = {
 		'shop_info': Shop.objects.all()[0],
-		'category': Category.objects.get(pk=category_id)
+		'category': Category.objects.get(pk=category_id),
+		'categories': Category.objects.all().filter(parent_category=None),
 	}
 	return render(request, 'category.html', response)
 
@@ -65,6 +67,7 @@ def categories(request):
 def contact(request):
 	response = {
 		'shop_info': Shop.objects.all()[0],
-		'categories': Category.objects.all().filter(parent_category=None)
+		'categories': Category.objects.all().filter(parent_category=None),
+		'categories': Category.objects.all().filter(parent_category=None),
 	}
 	return render (request, 'contact.html', response)
